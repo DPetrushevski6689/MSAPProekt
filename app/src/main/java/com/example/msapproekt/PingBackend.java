56b1a2959a9592515f77759270fdf10ca7cf8229
DPetrushevski6689 <david.petrusevski@gmail.com>
2020-05-23 17:04:17 +0200

    Finalna faza

--- app/src/main/java/com/example/msapproekt/PingBackend.java
+++ app/src/main/java/com/example/msapproekt/PingBackend.java
@@ -41,7 +41,62 @@ public class PingBackend extends AsyncTask<Void,Void,String> {
 
             for(int i=0;i<Array.length();i++)
             {
+
                    JSONObject currentItem = Array.getJSONObject(i);
+
+                   if(currentItem.getString("jobType").contains("PING"))
+                   {
+                       Log.d(LOG_TAG,"json = "+currentItem.toString());
+                       Log.d(LOG_TAG,"type = "+currentItem.getString("jobType"));
+                       Log.d(LOG_TAG,"host = "+currentItem.getString("host"));
+                       Log.d(LOG_TAG,"count = "+currentItem.getString("count"));
+                       Log.d(LOG_TAG,"packetSize = "+currentItem.getString("packetSize"));
+                       Log.d(LOG_TAG,"Period = "+currentItem.getString("jobPeriod"));
+                       Log.d(LOG_TAG,"date = "+currentItem.getString("date"));
+
+
+
+                       String pingCmd = "ping  -c  " + currentItem.getString("count");
+                       pingCmd = pingCmd + " -s " +currentItem.getString("packetSize");
+                       pingCmd = pingCmd +" " + currentItem.getString("host");
+                       String pingResult = "";
+                       Runtime r = Runtime.getRuntime();
+                       Process p = r.exec(pingCmd);
+                       BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                       String inputLine;
+                       while ((inputLine = in.readLine()) != null) {
+                           pingResult += inputLine;
+                       }
+                       in.close();
+                       Log.d(LOG_TAG,"pingResult "+pingResult);
+                       Service.pingResult = pingResult;
+                   }
+                   else if(currentItem.getString("jobType").contains("STAT"))
+                   {
+                       String returnString = null;
+                       String statResult = "";
+
+                       Process pstat = Runtime.getRuntime().exec("top -n 1");
+                       BufferedReader in = new BufferedReader(new InputStreamReader(pstat.getInputStream()));
+                       String inputLine;
+
+                       while (returnString==null||returnString.contentEquals("")){
+                           returnString = in.readLine();
+                       }
+                       statResult += returnString +",";
+                       while ((inputLine = in.readLine()) != null){
+                           inputLine += ";";
+                           statResult += inputLine;
+                       }
+                       in.close();
+                       if(pstat!=null){
+                           pstat.getOutputStream().close();
+                           pstat.getInputStream().close();
+                           pstat.getErrorStream().close();
+                       }
+                       Log.i("STAT TYPE","statResult = "+statResult);
+                   }
+                   /**
                    Log.d(LOG_TAG,"json = "+currentItem.toString());
                    Log.d(LOG_TAG,"type = "+currentItem.getString("jobType"));
                    Log.d(LOG_TAG,"host = "+currentItem.getString("host"));
@@ -65,7 +120,7 @@ public class PingBackend extends AsyncTask<Void,Void,String> {
                 }
                 in.close();
                 Log.d(LOG_TAG,"pingResult "+pingResult);
-                Service.pingResult = pingResult;
+                Service.pingResult = pingResult;**/
             }
         } catch (JSONException | IOException e) {
             e.printStackTrace();
